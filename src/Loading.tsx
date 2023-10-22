fd96fe9f34d411d61584d01f27347161703c30e0
Afzal Nomani <afzalnomani1@gmail.com>
2023-10-22 08:14:32 +0530

    tailwind loading component

---
+++ src/Loading.tsx
@@ -0,0 +1,16 @@
+function Loading() {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <div
+        className="h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
